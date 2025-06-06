eb21caaa27026f7606f3f76ae8fa8f501567386b
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-06-06 22:38:26 +0300

    arithmetic statements

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = (num1 * num2);
+
+console.log(multiply);
+
+let random ;
+random = Math.ceil(Math.random() * 10);
+
+console.log(random);
+
+let mod;
+
+let num3 = 10;
+let num4 = 6;
+
+mod = (num3 % num4);
+console.log(mod);
+
+let max;
+
+max = (Math.max(1,3,8,15,20,17,10));
+console.log(max);
